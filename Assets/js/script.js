5f9f09802edc6542178aab541541d1cad5df6cf0
hdela <hfdelacr@ucsd.edu>
2022-07-19 18:23:44 -0700

    Fixing up the javascript. Have rows with buttons and timeslots. Need to fix width and add on click event handler

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,36 @@
-//these two are jQuery objects. ---
+//=these two are jQuery objects. ---
 var containerEl = $(`.container`);
 var currentDayEl = $(`#currentDay`);
 var headerEl = $(`:Header`);
 
 
 let timesArray = [ "9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+let timesNumArray = [9, 10, 11, 12, 1, 2, 3, 4, 5]
 
 
 //need a function to add todays date/time
 function updateHeader(){
     //show current day and time
-     currentDayEl.text(moment().format('LLLL'));
+     currentDayEl.text(moment().format('dddd, MMMM Do YYYY, h:mm:ss a'));
+     setTimeout(updateHeader, 1000);
+     for (var i = 0; i<timesArray.length; i++){
+
+     }
 }
 
 $(document).ready(function(){
+    grandCreation();
+})
+
+
+//function to create the "save" button on 3rd column on every row. 
+function grandCreation(){
     for(var i = 0; i<timesArray.length; i++){
         var rowEl = $('<Form>');
         rowEl.attr("id", timesArray[i]);
         //creation of Text Area
         var textAreaEl = $(`<textarea>`);
-        textAreaEl.attr("id",timesArray[i]);
+        textAreaEl.attr("id",timesNumArray[i]);
         textAreaEl.text("Default");
         //add class for css properties along with text and ID
         rowEl.addClass('row').attr("id",timesArray[i]).text(timesArray[i]);
@@ -30,6 +41,7 @@ $(document).ready(function(){
          var saveBtnEl = $(`<button>`);
          //give em a class
          saveBtnEl.addClass("saveBtn");
+         saveBtnEl.attr("id", timesNumArray[i]);
          //give it some text
          saveBtnEl.text("Save");
          //add it to Row. 
@@ -37,13 +49,8 @@ $(document).ready(function(){
          //physically add rowEl to container to show in HTMl. 
         containerEl.append(rowEl);
         console.log(timesArray[i])
-
     }
-})
-
-
-//function to create the "save" button on 3rd column on every row. 
-
+}
 //function to dynamically update colors of column 2 in every row. 
 
 //call earlier functions to show up on page. 
